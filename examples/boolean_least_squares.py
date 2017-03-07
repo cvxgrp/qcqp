ef05d833b0fcf14f40b7996478e8522637ab3c23
Jaehyun Park <jpark@cs.stanford.edu>
2017-03-06 23:41:02 -0800

    adding missing files

--- examples/boolean_least_squares.py
+++ examples/boolean_least_squares.py
@@ -13,24 +13,23 @@ x = cvx.Variable(n)
 obj = cvx.sum_squares(A*x - b)
 cons = [cvx.square(x) == 1]
 prob = cvx.Problem(cvx.Minimize(obj), cons)
+qcqp = QCQPWrapper(prob)
 
-qprob = QCQPWrapper(prob)
-
-qprob.suggest(sdp=True, solver=cvx.MOSEK)
-print("SDP-based lower bound: %.3f" % qprob.sdp_bound)
-f_cd, v_cd = qprob.improve(COORD_DESCENT)
+qcqp.suggest(sdp=True, solver=cvx.MOSEK)
+print("SDP-based lower bound: %.3f" % qcqp.sdp_bound)
+f_cd, v_cd = qcqp.improve(COORD_DESCENT)
 x_cd = x.value
 print("Coordinate descent: objective %.3f, violation %.3f" % (f_cd, v_cd))
 print(x_cd.T)
 
-qprob.suggest(sdp=True) # SDP result is cached, not solved again
-f_dccp, v_dccp = qprob.improve(DCCP)
+qcqp.suggest(sdp=True) # SDP result is cached, not solved again
+f_dccp, v_dccp = qcqp.improve(DCCP)
 x_dccp = x.value
 print("Penalty CCP: objective %.3f, violation %.3f" % (f_dccp, v_dccp))
 print(x_dccp.T)
 
-qprob.suggest(sdp=True)
-f_admm, v_admm = qprob.improve(ADMM)
+qcqp.suggest(sdp=True)
+f_admm, v_admm = qcqp.improve(ADMM)
 x_admm = x.value
 print("Nonconvex ADMM: objective %.3f, violation %.3f" % (f_admm, v_admm))
 print(x_admm.T)
---
+++ qcqp/settings.py
@@ -0,0 +1,4 @@
+COORD_DESCENT = "coord-descent"
+ADMM = "admm"
+DCCP = "dccp"
+available_methods = [COORD_DESCENT, ADMM, DCCP]
