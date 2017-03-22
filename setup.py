7c1874dd029e7ece7d2f8afa14d76483b95523b7
Jaehyun Park <jpark@cs.stanford.edu>
2017-03-22 15:11:43 -0700

    bumping version

--- setup.py
+++ setup.py
@@ -2,15 +2,14 @@ from setuptools import setup
 
 setup(
     name='qcqp',
-    version='0.6',
+    version='0.7',
     author='Jaehyun Park, Stephen Boyd',
     author_email='jpark@cs.stanford.edu, boyd@stanford.edu',
     packages=['qcqp'],
     license='MIT',
     zip_safe=False,
     install_requires=["cvxpy >= 0.4.8",
-                      "CVXcanon >= 0.1.1",
-                      "joblib >= 0.10.3"],
+                      "CVXcanon >= 0.1.1"],
     use_2to3=True,
     url='http://github.com/cvxgrp/qcqp/',
     description='CVXPY extension for nonconvex QCQPs.',
