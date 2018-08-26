81174dfb5f2364c4459a23d607f8a5493ae700f8
Jaehyun Park <jaehyun@fb.com>
2018-08-25 22:23:00 -0700

    require cvxpy 0.4

--- setup.py
+++ setup.py
@@ -2,13 +2,14 @@ from setuptools import setup
 
 setup(
     name='qcqp',
-    version='0.8.2',
+    version='0.8.3',
     author='Jaehyun Park, Stephen Boyd',
     author_email='jpark@cs.stanford.edu, boyd@stanford.edu',
     packages=['qcqp'],
     license='MIT',
     zip_safe=False,
     install_requires=["cvxpy >= 0.4.8",
+                      "cvxpy < 1.0",
                       "CVXcanon >= 0.1.1"],
     use_2to3=True,
     url='http://github.com/cvxgrp/qcqp/',
