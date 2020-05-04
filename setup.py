d30f3a64b1289de9428e96f80f73a82e77f167e4
Danial Dervovic <dannydervovic@hotmail.com>
2020-05-04 16:25:24 +0100

    setup.py to include `scipy` version

--- setup.py
+++ setup.py
@@ -10,6 +10,7 @@ setup(
     zip_safe=False,
     install_requires=["cvxpy >= 0.4.8",
                       "cvxpy < 1.0",
+                      "scipy <= 1.1.0",
                       "CVXcanon >= 0.1.1"],
     use_2to3=True,
     url='http://github.com/cvxgrp/qcqp/',
